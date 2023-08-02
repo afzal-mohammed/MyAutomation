be7762da6872dcdb1141d5f5cfa91a62e6bc1ae6
Asad <asadthegift.ak@gmail.com>
2023-08-02 15:39:15 +0530

    Made new package JavaLearning for java programms & made EvenOdd program

---
+++ src/main/java/JavaLearning/NumberEvenOdd.java
@@ -0,0 +1,24 @@
+package JavaLearning;
+
+public class NumberEvenOdd {
+
+	static void display(int x) {
+
+		if (x % 2 == 0) {
+
+			System.out.println("Given number is even " + x);
+
+		} else {
+
+			System.out.println("Given number is odd " + x);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		NumberEvenOdd ab = new NumberEvenOdd();
+
+		display(7);
+	}
+
+}
