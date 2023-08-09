d2a5722d36b98453398f8c0d6f540eb220435664
Asad-khan46 <asadthegift.ak@gmail.com>
2023-08-09 16:58:57 +0530

    Created program to reverse number

---
+++ src/main/java/JavaLearning/ReverseNumber.java
@@ -0,0 +1,18 @@
+package JavaLearning;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+
+		int num = 123;
+		int rev;
+
+		while (num > 0) {
+			rev = num % 10;
+			System.out.print(rev);
+			num = num / 10;
+
+		}
+	}
+
+}
